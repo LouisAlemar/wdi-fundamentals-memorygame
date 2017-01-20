645c270c3f5dea5f63086c11103a7c23b28e4149
Louis Alemar <louisalemar@Louiss-Device.fios-router.home>
2017-01-19 23:44:58 -0500

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
+
+// Declare variables for cards
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
